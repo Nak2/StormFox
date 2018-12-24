7e7d17d583df3d3ded6a0fe47f45a278b885395e
UAVXP <uavxp29@gmail.com>
2018-12-24 06:06:29 +0200

    Added Russian localization

---
+++ lua/stormfox/language/ru.lua
@@ -0,0 +1,162 @@
+#[[	Stormfox Russian
+
+#Server Settings
+	sf_description.autopilot=Пытаться исправлять все проблемы при запуске
+	sf_description.timespeed=секунд игрового времени в одну секунду реального
+	sf_description.moonscale=Масштаб Луны
+	sf_description.moonphase=Включить смену фаз Луны
+	sf_description.enablefog=Разрешить SF изменять настройки тумана
+	sf_description.weatherdebuffs=Включить урон от погоды (поражение молнией, радиацией)
+	sf_description.windpush=Включить движение пропов от ветра
+	sf_description.lightningbolts=Включить молнию
+	sf_description.enable_mapsupport=Включить поддержку специальных энтить окружения
+	sf_description.sunmoon_yaw=Расположение Солнца/Луны
+	sf_description.debugcompatibility=Включить отладку совместимости (для экспертов)
+	sf_description.skybox=Включить генерацию неба
+	sf_description.enable_ekstra_lightsupport=Вкл. улучшенное освещение(лагает на больших картах)
+	sf_description.start_time=Запуск сервера с текущим временем
+	sf_description.mapbloom=Разрешить SF управлять эффектами блума
+	sf_description.enable_mapbrowser=Разрешить админам сменять карты в Браузере карт
+	sf_description.allowcl_disableeffects=Разрешить клиентам отключать эффекты в SF
+	sf_description.autoweather=Включить автоматическую смену погоды
+	sf_description.block_lightenvdelete=Set light_environment's targetname.
+	sf_description.realtime=Следовать локальному времени сервера
+	sf_description.foliagesway=Включить раскачивание листвы деревьев
+	sf_description.disableambient_sounds=Отключить звуки окружения карты
+	sf_description.sf_enable_ekstra_entsupport=Обновлять все энтити при смене освещения
+
+#Client Settings
+	sf_description.disableeffects=Отключить все эффекты
+	sf_description.exspensive=[0-7+] Уровень качества погодных эффектов
+	sf_description.exspensive_fps=Уровень качества эффектов на основе FPS
+	sf_description.exspensive_manually=Ручная настройка:
+	sf_description.material_replacment=Включить замену текстур на карте (снег, песок)
+	sf_description.allow_rainsound=Включить звуки дождя
+	sf_description.allow_windsound=Включить звуки ветра
+	sf_description.allow_dynamiclights=Разрешить светиться лампам из SF
+	sf_description.allow_sunbeams=Включить солнечные лучи
+	sf_description.allow_dynamicshadow=Включить динамическое освещение/тени
+	sf_description.dynamiclightamount=Уровень динамического освещения:
+	sf_description.redownloadlightmaps=Обнов. лайтмап(лагает на больших картах)
+	sf_description.allow_raindrops=Включить капли дождя на экране
+	sf_description.renderscreenspace_effects=Разрешить изменение цветов экрана
+	sf_description.useAInode=Использовать AI ноды для улучшения качества погодных эффектов
+	sf_description.enable_breath=Включить облако пара при дыхании
+	sf_description.enable_windoweffect=Включить капли дождя на разбиваемых окнах
+	sf_description.enable_windoweffect_enable_tr=Проверять, попадает ли дождь на стекло
+	sf_description.rainpuddle_enable=Включить появление луж от дождя (требуются CS:GO и AI ноды)
+	sf_description.footsteps_enable=Отображать следы от ног на снегу
+	sf_description.footsteps_max=Количество следов
+	sf_description.footsteps_distance=Дальность прорисовки следов
+
+	sf_description.hq_shadowmaterial=Включить высококачественные тени
+
+#Map Settings
+	sf_description.dynamiclight=Разрешить динамическое освещение для всех клиентов
+	sf_description.replace_dirtgrassonly=Заменять только текстуры травы/земли
+	sf_description.wind_breakconstraints=Разрывать крепления и расфризить пропы при сильном ветре
+
+#StormFox msg
+	sf_added_content=%i файлов добавлено для скачки клиентами
+	sf_running_current=Используется актуальная версия.
+	sf_running_old=Вы используете устаревшую версию StormFox.
+	sf_running_new=Вы используете версию StormFox новее, чем на Workshop. Будьте готовы к непредсказуемым багам.
+	sf_running_old.console=Устаревшая версия. Версия на Workshop: %s
+	sf_running_version=You're running: %i
+
+#MapData
+	sf_mapdata_load=Загружены данные для карты
+	sf_mapdata_invalid=С сервера получены неверные данные карты!
+	sf_mapdata_cleanup=Очистка изменений карты...
+	sf_ain_load=Загружен файл AI нодов
+
+#Weather
+	sf_current_weather=Текущая погода
+	Rain=Дождь
+	Raining=Идёт дождь
+	Radioactive=Радиация
+	Sandstorm=Песчаная буря
+	Cloudy=Облачно
+	Clear=Ясно
+	Fog=Туман
+	Light Fog=Лёгкий туман
+	Storm=Шторм
+	Snowing=Идёт снег
+	Thunder=Гроза
+
+#Wind
+	Calm=Штиль
+	Light Air=Тихий ветер
+	Light Breeze=Лёгкий бриз
+	Gentle Breeze=Ощутимый бриз
+	Moderate Breeze=Умеренный бриз
+	Fresh Breeze=Свежий ветер
+	Strong Breeze=Сильный ветер
+	Near Gale=Лёгкий шторм
+	Gale=Шторм
+	Strong Gale=Сильный шторм
+	Violent Storm=Интенсивный шторм
+	Hurricane=Ураган
+	Category 2=Категория 2
+	Category 3=Категория 3
+	Category 3=Категория 4
+	Category 5=Категория 5
+
+#Interface basics
+	Settings=Настройки
+	Server Settings=Настройки сервера
+	Client Settings=Клиентские настройки
+	Controller=Управление
+	Troubleshooter=Помощник
+	Reset=Сброс
+	Weather Controller=Управление погодой
+	Effects=Эффекты
+	Map Browser=Браузер карт
+	Map=Карта
+	Clients=Игроки
+	Other=Прочее
+	Time=Время
+	Misc=Прочее
+	Weather=Погода
+	Auto Weather=Авто-погода
+	Adv Light=Освещение (расш.)
+	Sun / Moon=Солнце / Луна
+	Changelog=Список изменений
+	Temperature=Температура
+	Wind=Ветер
+	Quality=Качество
+	Materials=Текстуры
+	Rain/Snow Effects=Эффекты дождя/снега
+
+#Interface adv	
+	sf_troubleshooter.description=Здесь перечислены основные проблемы с настройкой SF
+	sf_temperature_range=Пределы изменения температуры
+	sf_setwindangle=Направл. ветра
+	sf_setweather=Уст. погоду
+	sf_settime=Установить время
+	sf_holdc=Зажмите C
+	sf_interface_lighttheme=Светлая тема
+	sf_interface_darktheme=Тёмная тема
+	sf_interface_light=Освещение
+	sf_interface_light_range=Пределы освещённости: 
+	sf_interface_save_on_exit=Установить
+	sf_interface_adv_light=Adv light
+	sf_interface_closechat=Закройте чат
+	sf_interface_closeconsole=Закройте консоль
+	sf_interface_material_replacment=Работа с текстурами карты
+	sf_interface_max_wind=Макс.
+	sf_interface_max_footprints=Количество следов
+	sf_interface_footprint_render=Дальность прорисовки следов
+
+#Errors and warning
+	sf_missing_convar=Отсутствующий convar
+	sf_warning_clientlag=Может вызывать лаги на клиентах!
+	sf_warning_serverlag=Может вызвать серьёзный лаг на сервере!
+	sf_warning_reqmapchange=нужна смена карты
+	sf_description.disabled_on_server=невозможно на этом сервере
+	sf_warning_unsupportmap=треб. для неподдерж. карт
+	sf_warning_missingmaterial.title=Отсутствуют необходимые текстуры
+	sf_warning_missingmaterial.nevershow=Никогда больше не показывать
+	sf_warning_missingmaterial=У Вас не хватает %i текстур(ы)
+	sf_warning_unfinished_a = На данный момент Помощник можно вызвать только из серверной панели SF.
+	sf_warning_unfinished_b = Открыть серверный Помощник (требуется разрешение)
