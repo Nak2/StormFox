ea265c0e777cf31bcf2e31274d30afdd1cfb5bde
IBRS <443817949@qq.com>
2023-02-17 18:52:20 +0800

    CN Translation

---
+++ lua/stormfox/language/zh-cn.lua
@@ -0,0 +1,193 @@
+return [[Stormfox Simplified Chinese
+
+#StormFox
+	sf_description.newversion = 你正在运行测试版本的 SF
+	sf_description.oldversion = 你正在运行老版本的 SF
+
+#Tool
+	sf_tool.menu 		= 菜单
+	sf_tool.maptexture 	= 地图贴图
+	sf_tool.maptexture.helpm1 = 按下鼠标左键来添加材质
+	sf_tool.maptexture.helpm2 = 按下鼠标右键来移除材质
+	sf_tool.permaentity = Perma 实体
+	sf_tool.addmaterial = 添加
+	sf_tool.cancel 		= 取消
+	sf_tool.menu_reload = 按下 R 键打开菜单
+
+#Variables
+	sf_type.roof 		= 屋顶
+	sf_type.dirtgrass 	= 草地/泥土
+	sf_type.road 		= 公路
+	sf_type.pavement 	= 人行道
+
+	# - Weather
+		sf_weather.clear 		= 晴天
+		sf_weather.rain 		= 雨天
+		sf_weather.raining 		= 下雨
+		sf_weather.sleet 		= 雪雨
+		sf_weather.snowing 		= 下雪
+		sf_weather.fog 			= 雾天
+		sf_weather.light_fog 	= 轻雾
+		sf_weather.heavy_fog 	= 大雾
+		sf_weather.storm 		= 风暴
+		sf_weather.thunder 		= 雷暴
+		sf_weather.cloudy 		= 多云
+		sf_weather.lava 		= 熔岩
+		sf_weather.lava_eruption= 熔岩喷发
+		sf_weather.sandstorm 	= 沙尘暴
+		sf_weather.radioactive 	= 辐射
+		sf_weather.radioactive_rain= 辐射雨
+
+	# - Wind
+		sf_winddescription.calm 			= 平静
+		sf_winddescription.light_air		= 一级风
+		sf_winddescription.light_breeze		= 二级风
+		sf_winddescription.gentle_breeze	= 三级风
+		sf_winddescription.moderate_breeze	= 四级风
+		sf_winddescription.fresh_breeze		= 五级风
+		sf_winddescription.strong_breeze	= 强风
+		sf_winddescription.near_gale		= 特强风
+		sf_winddescription.gale				= 飓风
+		sf_winddescription.strong_gale		= 烈风
+		sf_winddescription.storm			= 风暴
+		sf_winddescription.violent_storm	= 强风暴
+		sf_winddescription.hurricane		= 飓风
+		sf_winddescription.cat2				= 二级飓风
+		sf_winddescription.cat3				= 三级飓风
+		sf_winddescription.cat4				= 四级飓风
+		sf_winddescription.cat5				= 五级飓风
+
+#Weather
+	sf_current_weather = Current Weather
+
+#Server Settings
+	sf_description.autopilot		 = 在启动时尝试修复全部问题。
+	sf_description.timespeed		 = 秒的游戏内时间/现实时间。
+	sf_description.moonscale		 = 月尺。
+	sf_description.moonphase 		 = 启用月相。
+	sf_description.enablefog 		 = 允许 SF 编辑雾气。
+	sf_description.weatherdebuffs 	 = 启用天气带来的负面效果/伤害/冲击。
+	sf_description.windpush 		 = 启用风推动物品。
+	sf_description.lightningbolts 	 = 启用闪电火花。
+	sf_description.enable_mapsupport = 启用地图的实体支持。
+	sf_description.sunmoon_yaw 	 	 = 太阳/月亮角度。
+	sf_description.debugcompatibility = 启用 SF 兼容性调试器。
+	sf_description.skybox 			 = 启用 SF 天空盒。
+	sf_description.enable_ekstra_lightsupport = 启用额外的光照支持（在大型地图上会造成卡顿）
+	sf_description.start_time 		 = 以特定的时间启动服务器。
+	sf_description.mapbloom 		 = 允许 SF 编辑光晕。
+	sf_description.enable_mapbrowser = 允许管理员通过 SF 地图浏览器更改地图。
+	sf_description.allowcl_disableeffects = 允许客户端禁用 SF 特效。
+	sf_description.autoweather 		 = 启用天气生成。
+	sf_description.realtime 		 = 跟随本地时间。
+	sf_description.foliagesway 		 = 启用树叶晃动。
+	sf_description.override_soundscape = 覆盖地图音效。
+	sf_description.sf_enable_ekstra_entsupport = 光照变化时更新全部实体。（高服务器负担）
+
+#Client Settings
+	sf_description.disableeffects 		= 禁用所有特效。
+	sf_description.exspensive 			= [0-7+] 启用高质量天气计算。
+	sf_description.exspensive_fps 		= 根据 FPS 动态调整天气质量。
+	sf_description.exspensive_manually 	= 手动调整天气质量。
+	sf_description.material_replacment 	= 启用天气特效的材质替换。
+	sf_description.allow_rainsound 		= 启用雨水音效。
+	sf_description.allow_windsound 		= 启用刮风音效。
+	sf_description.allow_dynamiclights 	= 启用 SF 的灯光照。
+	sf_description.allow_sunbeams 		= 启用阳光特效。
+	sf_description.allow_dynamicshadow 	= 启用动态光照/阴影。
+	sf_description.dynamiclightamount 	= 控制动态光照的数量。
+	sf_description.redownloadlightmaps 	= 更新光照贴图（在大型地图上会造成卡顿）
+	sf_description.allow_raindrops 		= 启用屏幕上的雨点特效
+	sf_description.renderscreenspace_effects = 启用 RenderScreenspaceEffects
+	sf_description.useAInode 			= 使用 AI 节点获得更真实的声音和特效。
+	sf_description.enable_breath 		= 启用喘出热气特效。
+	sf_description.enable_windoweffect 	= 启用可破坏窗户上的雨滴特效。
+	sf_description.enable_windoweffect_enable_tr = 检测雨滴是否碰到可破坏窗户。
+	sf_description.rainpuddle_enable 	= 雨天产生水坑。（需要 AI 节点）
+	sf_description.footsteps_enable 	= 在雪地中渲染脚印。
+	sf_description.footsteps_max 		= 最大脚印数量。
+	sf_description.footsteps_distance 	= 雪中足迹的渲染距离。
+	sf_description.hq_shadowmaterial 	= 设置高清阴影参数
+
+#Map Settings
+	sf_description.map_entities			 = 地图实体
+	sf_description.dynamiclight 		 = 允许所有客户端使用动态光照。
+	sf_description.replace_dirtgrassonly = 仅替换草地/泥土。
+	sf_description.wind_breakconstraints = 强风可以破坏约束并解冻物品。
+
+#StormFox msg
+	sf_added_content 			= 已添加 %i 个内容文件
+	sf_permisson.deny 			= 你没有权限更改天气设置
+	sf_permisson.denysettings 	= 你没有权限更改 SF 设置
+	sf_permisson.denymap 		= 你没有权限更改地图
+	sf_permisson.denymapsetting = 此服务器已禁用地图浏览器
+	sf_permisson.denymapmissing = 服务器缺少该地图
+	sf_generating.puddles 		= 已生成水坑位置
+	sf_missinglanguage 			= 缺少翻译文件：
+
+#MapData
+	sf_mapdata_load 	 = 已加载地图数据。
+	sf_mapdata_invalid 	 = 来自服务器的地图数据无效！
+	sf_mapdata_cleanup 	 = 正在清除地图更改...
+	sf_ain_load 		 = 已加载 .ain 文件
+
+#Interface basics
+	Settings 		 = 设置
+	Server Settings  = 服务器设置
+	Client Settings  = 客户端设置
+	Controller 		 = 控制器
+	Troubleshooter 	 = 问题解答
+	Reset 			 = 重置
+	Weather Controller = 天气控制器
+	Effects 		 = 特效
+	Map Browser 	 = 地图浏览器
+	Map 			 = 地图
+	Clients 		 = 客户端
+	Other 			 = 其它
+	Time 			 = 时间
+	Misc 			 = 杂项
+	Weather 		 = 天气
+	Auto Weather 	 = 自动调整天气
+	Adv Light 		 = 高级光照
+	Sun / Moon 		 = 太阳 / 月亮
+	Changelog 		 = 更新日志
+	Temperature 	 = 气温
+	Wind 			 = 风力
+	Quality 		 = 质量
+	Materials 		 = 材质
+	Rain/Snow Effects= 雨/雪特效
+
+#Interface adv	
+	sf_troubleshooter.description 	= 这将显示当前设置可能会出现的常见问题。
+	sf_temperature_range 		 	= 气温范围
+	sf_setwindangle 			 	= 设置风向
+	sf_setweather 					= 设置天气
+	sf_settime 						= 设置时间
+	sf_holdc 						= 按住 C 键
+	sf_interface_lighttheme 		= 浅色模式
+	sf_interface_darktheme 			= 深色模式
+	sf_interface_light 				= 光照
+	sf_interface_light_range 		= 光照范围：
+	sf_interface_save_on_exit 		= 退出时保存
+	sf_interface_adv_light 			= 高级光照
+	sf_interface_closechat 			= 关闭聊天框来互动
+	sf_interface_closeconsole 		= 关闭控制台
+	sf_interface_material_replacment= 材质替换
+	sf_interface_max_wind 			= 最大风力
+	sf_interface_max_footprints 	= 脚印上限
+	sf_interface_footprint_render 	= 脚印渲染距离
+	sf_interface_language 			= 语言设置
+
+#Errors and warning
+	sf_missing_convar 				= 缺失指令
+	sf_warning_clientlag 			= 可能造成部分客户端卡顿！
+	sf_warning_serverlag 			= 可能造成严重的服务器卡顿！
+	sf_warning_reqmapchange 		= 需要更改地图
+	sf_description.disabled_on_server = 此服务器已禁用
+	sf_warning_unsupportmap 		= 用于不受支持的地图
+	sf_warning_missingmaterial.title = 你缺少必要的材质。
+	sf_warning_missingmaterial.nevershow = 不再显示。
+	sf_warning_missingmaterial 		= 你缺少 %i 个材质。
+	sf_warning_unfinished_a 		= 此功能尚未完成。请使用“server-settings（服务器设置）”中的“问题解答”
+	sf_warning_unfinished_b 		= 打开服务器端的“问题解答”（需要权限）
+]]
